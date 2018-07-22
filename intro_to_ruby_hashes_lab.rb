0ce9124e07684c5c05b3dbbe2e20661e0abfb2e3
Sherzod Karimov <karimovj@gmail.com>
2018-07-22 20:22:42 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -41,7 +41,7 @@ def monopoly_with_fourth_tier
     	monopoly = {
 	  :railroads => {:pieces => 4, 
 	  :names => {
-	     :reading_railroad => {"mortage_value" => 100}, 
+	     :reading_railroad => {"mortage_value" => "100"}, 
 	     :pennsylvania_railroad => {"mortage_value" => 200}, 
 	     :b_and_o_railroad => {"mortage_value" => 400}, 
 	     :shortline => {"mortage_value" => 800},
